491a4ad606a05afa5860e74c979df54a8c997931
Thiago Vieira de Carvalho <thiago.carvalho@unimedvr.com.br>
2024-04-24 19:18:49 -0300

    Fix - add vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="page1">
+    <h1 class="love">jainy, amo voce <3</h1>
+  </div>
+  <div id="page2">
+    <p class="love">oi, meu amor</p>
+  </div>
+  <div id="page3">
+    <p class="love">meu amor da minha vida <3</p>
+  </div>
+</template>
+
+<script></script>
